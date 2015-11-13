da939691a65d1ee068955c787628768278897b36
Martin Kolar <martin@igloonet.cz>
2015-11-13 14:25:52 +0100

    switch on/off on layer menu

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // @include             https://editor-beta.waze.com/*
 // @include             https://www.waze.com/editor/*
 // @include             https://www.waze.com/*/editor/*
-// @version             0.1.2
+// @version             0.1.3
 // @grant               none
 // ==/UserScript==
 
@@ -15,140 +15,169 @@
 
 // global variables
 
-var wmeSpeedsVersion = "0.1";
+var wmeSpeedsVersion = "0.1.3";
 var wmeSpeedsInit = false;
 var wmeSpeedsColors = ['#f00', '#321325', '#540804', '#BA1200', '#FA4A48', '#F39C6B', '#A7D3A6', '#ADD2C2', '#CFE795', '#F7EF81', '#BDC4A7', '#95AFBA', '#3F7CAC', '#0A369D', '#001C55'];
 var wmeSpeedsMaxSpeed = 131;
+var wmeSpeedsLayer;
 
 
 /* =========================================================================== */
-function highlightSegments(event) {
-  for (var seg in Waze.model.segments.objects) {
-    var segment = Waze.model.segments.get(seg);
-    var attributes = segment.attributes;
-    var line = getId(segment.geometry.id);
+function highlightSpeedsSegments(event) {
+  // console.log('highlightSpeedsSegments');
 
-    if (line === null) {
-      continue;
-    }
+  if (wmeSpeedsLayer.getVisibility()) {
+    for (var seg in Waze.model.segments.objects) {
+      var segment = Waze.model.segments.get(seg);
+      var attributes = segment.attributes;
+      var line = getId(segment.geometry.id);
 
-    // check that WME hasn't highlighted this segment
-    var opacity = line.getAttribute("stroke-opacity");
-    var lineWidth = line.getAttribute("stroke-width");
-
-    if (opacity == 1 || lineWidth == 9)
-      continue;
-
-    // turn off highlights when roads are no longer visible
-    var roadType = attributes.roadType;
-    if (Waze.map.zoom <= 3 && (roadType < 2 || roadType > 7)) {
-      if (opacity > 0.1) {
-        line.setAttribute("stroke","#dd7700");
-        line.setAttribute("stroke-opacity",0.001);
-        line.setAttribute("stroke-dasharray", "none");
-      }
-      continue;
-    }
-
-    // get current state of the line
-    var lineColor = line.getAttribute("stroke");
-
-    // default colours
-    var newColor = "#dd7700";
-    var newOpacity = 0.001;
-    var newDashes = "none";
-    var newWidth = 8;
-
-    if (roadType != 5 && roadType != 10) {
-      newColor = "#fff";
-      newOpacity = 1;
-      newWidth = 8;
-
-      speed1allow = attributes.fwdDirection;
-      speed2allow = attributes.revDirection;
-
-      if (!speed1allow && !speed2allow) {
+      if (line === null) {
         continue;
       }
 
-      if (speed1allow && typeof attributes.fwdMaxSpeed == 'number') {
-        if (attributes.fwdMaxSpeed >= wmeSpeedsMaxSpeed) {
-          speed1 = Math.ceil(wmeSpeedsMaxSpeed / 10);
-        }
-        else {
-          speed1 = Math.ceil(attributes.fwdMaxSpeed / 10);
-        }
-      }
-      else {
-        speed1 = 0;
-      }
+      // check that WME hasn't highlighted this segment
+      var opacity = line.getAttribute("stroke-opacity");
+      var lineWidth = line.getAttribute("stroke-width");
 
-      if (speed2allow && typeof attributes.revMaxSpeed == 'number') {
-        if (attributes.revMaxSpeed >= wmeSpeedsMaxSpeed) {
-          speed2 = Math.ceil(wmeSpeedsMaxSpeed / 10);
-        }
-        else {
-          speed2 = Math.ceil(attributes.revMaxSpeed / 10);
-        }
-      }
-      else {
-        speed2 = 0;
-      }
+      if (opacity == 1 || lineWidth == 9)
+        continue;
 
-      speed1color = wmeSpeedsColors[speed1];
-      speed2color = wmeSpeedsColors[speed2];
-
-      if ((speed1 == 0  && speed2 == 0) || (!speed2allow && speed1 == 0) || (!speed1allow && speed2 == 0)) {
+      // turn off highlights when roads are no longer visible
+      var roadType = attributes.roadType;
+      if (Waze.map.zoom <= 3 && (roadType < 2 || roadType > 7)) {
+        if (opacity > 0.1) {
+          line.setAttribute("stroke","#dd7700");
+          line.setAttribute("stroke-opacity",0.001);
+          line.setAttribute("stroke-dasharray", "none");
+        }
         continue;
       }
 
-      if (speed1allow && speed2allow) {
-        if (speed1 == 0 || speed2 == 0) {
-          newDashes = "10 10";
-          newColor = '#ff00ff';
-          newWidth = 9;
+      // get current state of the line
+      var lineColor = line.getAttribute("stroke");
+
+      // default colours
+      var newColor = "#dd7700";
+      var newOpacity = 0.001;
+      var newDashes = "none";
+      var newWidth = 8;
+
+      if (roadType != 5 && roadType != 10) {
+        newColor = "#fff";
+        newOpacity = 1;
+        newWidth = 8;
+
+        speed1allow = attributes.fwdDirection;
+        speed2allow = attributes.revDirection;
+
+        if (!speed1allow && !speed2allow) {
+          continue;
         }
-        else if (speed1 != speed2) {
-          if (speed1 < speed2) {
-            newColor = speed1color;
+
+        if (speed1allow && typeof attributes.fwdMaxSpeed == 'number') {
+          if (attributes.fwdMaxSpeed >= wmeSpeedsMaxSpeed) {
+            speed1 = Math.ceil(wmeSpeedsMaxSpeed / 10);
           }
           else {
-            newColor = speed2color;
+            speed1 = Math.ceil(attributes.fwdMaxSpeed / 10);
           }
-
-          newDashes = "10 10";
         }
         else {
+          speed1 = 0;
+        }
+
+        if (speed2allow && typeof attributes.revMaxSpeed == 'number') {
+          if (attributes.revMaxSpeed >= wmeSpeedsMaxSpeed) {
+            speed2 = Math.ceil(wmeSpeedsMaxSpeed / 10);
+          }
+          else {
+            speed2 = Math.ceil(attributes.revMaxSpeed / 10);
+          }
+        }
+        else {
+          speed2 = 0;
+        }
+
+        speed1color = wmeSpeedsColors[speed1];
+        speed2color = wmeSpeedsColors[speed2];
+
+        if ((speed1 == 0  && speed2 == 0) || (!speed2allow && speed1 == 0) || (!speed1allow && speed2 == 0)) {
+          continue;
+        }
+
+        // if ((!attributes.fwdDirection && typeof attributes.fwdMaxSpeed == 'number') || (!attributes.revDirection && typeof attributes.revMaxSpeed == 'number')) {
+        //   newDashes = "10 10";
+        //   newColor = '#ffffff';
+        //   newWidth = 9;
+
+        //   if (!attributes.fwdDirection) {
+        //     newColor = '#999999';
+        //   }
+        // }
+
+        else if (speed1allow && speed2allow) {
+          if (speed1 == 0 || speed2 == 0) {
+            newDashes = "10 10";
+            newColor = '#ff00ff';
+            newWidth = 9;
+          }
+          else if (speed1 != speed2) {
+            if (speed1 < speed2) {
+              newColor = speed1color;
+            }
+            else {
+              newColor = speed2color;
+            }
+
+            newDashes = "10 10";
+          }
+          else {
+            newColor = speed1color;
+          }
+        }
+
+        else if (speed1allow) {
           newColor = speed1color;
+        }
+        else {
+          newColor = speed2color;
+        }
+
+        if ((attributes.fwdDirection && attributes.fwdMaxSpeed == 126) || (attributes.revDirection && attributes.revMaxSpeed == 126)) {
+          newColor = "#000";
+          newOpacity = 1;
+          newWidth = 9;
         }
       }
 
-      else if (speed1allow) {
-        newColor = speed1color;
+      // if colour has changed, update the line attributes
+      if (lineColor != newColor) {
+        line.setAttribute("stroke", newColor);
+        line.setAttribute("stroke-opacity", newOpacity);
+        line.setAttribute("stroke-dasharray", newDashes);
+        if (newColor != "#dd7700") { //default
+          line.setAttribute("stroke-width", newWidth);
+        } else {
+          line.setAttribute("stroke-width", 6);
+        }
       }
-      else {
-        newColor = speed2color;
+    } // end of loop
+  }
+  else {
+    for (var seg in Waze.model.segments.objects) {
+      var segment = Waze.model.segments.get(seg);
+      var line = getId(segment.geometry.id);
+
+      if (line === null) {
+        continue;
       }
 
-      if ((attributes.fwdDirection && attributes.fwdMaxSpeed == 126) || (attributes.revDirection && attributes.revMaxSpeed == 126)) {
-        newColor = "#000";
-        newOpacity = 1;
-        newWidth = 9;
-      }
+      line.setAttribute("stroke","#dd7700");
+      line.setAttribute("stroke-opacity",0.001);
+      line.setAttribute("stroke-dasharray", "none");
     }
-
-    // if colour has changed, update the line attributes
-    if (lineColor != newColor) {
-      line.setAttribute("stroke", newColor);
-      line.setAttribute("stroke-opacity", newOpacity);
-      line.setAttribute("stroke-dasharray", newDashes);
-      if (newColor != "#dd7700") { //default
-        line.setAttribute("stroke-width", newWidth);
-      } else {
-        line.setAttribute("stroke-width", 6);
-      }
-    }
-  } // end of loop
+  }
 } // end of function
 
 /* helper function */
@@ -167,7 +196,7 @@ function getId(node) {
 }
 
 /* =========================================================================== */
-function initialiseHighlights()
+function initialiseSpeedsHighlights()
 {
   if (wmeSpeedsInit) {
     return;
@@ -176,14 +205,38 @@ function initialiseHighlights()
   // init shortcuts
   if(!window.Waze.map)
   {
-      window.console.log("WME Speeds "
-          + ": waiting for WME...");
-      setTimeout(initialiseHighlights, 555);
+      window.console.log("WME Speeds: waiting for WME...");
+      setTimeout(initialiseSpeedsHighlights, 555);
       return;
   }
 
+  if(typeof(unsafeWindow.OpenLayers) === 'undefined'){
+    console.log("WME Speeds: OpenLayers : NOK");
+    setTimeout(initialiseSpeedsHighlights, 500);
+    return;
+  }
+
+  if(typeof(unsafeWindow.Waze) === 'undefined'){
+    console.log("WME Speeds: Waze : NOK");
+    setTimeout(initialiseSpeedsHighlights, 500);
+    return;
+  }
+
+  wmeSpeedsLayer = new unsafeWindow.OpenLayers.Layer.Vector("Speeds", {
+      displayInLayerSwitcher: true,
+      uniqueName: "__DrawSegmentSpeeds"
+    });
+
+  I18n.translations.en.layers.name["__DrawSegmentSpeeds"] = "WME Speeds";
+  unsafeWindow.Waze.map.addLayer(wmeSpeedsLayer);
+  if (localStorage.DrawSegmentSpeeds) {
+    wmeSpeedsLayer.setVisibility(localStorage.DrawSegmentSpeeds == "true");
+  } else {
+    wmeSpeedsLayer.setVisibility(true);
+  }
+
   // begin periodic updates
-  window.setInterval(highlightSegments,333);
+  window.setInterval(highlightSpeedsSegments,333);
 
   // trigger code when page is fully loaded, to catch any missing bits
   window.addEventListener("load", function(e) {
@@ -192,13 +245,13 @@ function initialiseHighlights()
   });
 
   // register some events...
-  Waze.map.events.register("zoomend", null, highlightSegments);
+  Waze.map.events.register("zoomend", null, highlightSpeedsSegments);
 
   wmeSpeedsInit = true;
 }
 
 /* engage! =================================================================== */
-initialiseHighlights();
+initialiseSpeedsHighlights();
 
 })();
 /* end ======================================================================= */
