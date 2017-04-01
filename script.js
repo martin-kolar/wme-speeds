5416827b417e53ec9c6f5cc0274de53de5e93302
Martin Kolar <martin@igloonet.cz>
2017-04-01 11:36:01 +0200

    new version for new WME version

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 // @include             https://www.waze.com/*/editor/*
 // @exclude             https://www.waze.com/user/*editor/*
 // @exclude             https://www.waze.com/*/user/*editor/*
-// @version             0.4.3.3
+// @version             0.4.3.4
 // @grant               none
 // @contributor         FZ69617
 // ==/UserScript==
@@ -24,7 +24,7 @@
 
 // global variables
 
-var wmeSpeedsVersion = '0.4.3.3';
+var wmeSpeedsVersion = '0.4.3.4';
 var wmeSpeedsInit = false;
 var wmeSpeedsColors =    ['#ff0000', '#321325', '#540804', '#BA1200', '#FA4A48', '#F39C6B', '#A7D3A6', '#ADD2C2', '#CFE795', '#F7EF81', '#BDC4A7', '#95AFBA', '#3F7CAC', '#0A369D', '#001C55'];
 var wmeSpeedsColorsMph = ['#ff0000', '#321325', '#702632', '#540804', '#A00027', '#BA1200', '#F15872', '#FA4A48', '#F39C6B', '#A7D3A6', '#ADD2C2', '#CFE795', '#F7EF81', '#BDC4A7', '#95AFBA', '#3F7CAC', '#0A369D', '#001C55', '#000000'];
@@ -169,8 +169,9 @@ function highlightSpeedsSegments() {
   wmeSpeedsTransparentColors = getId('_wmeSpeedsTransparentColors').checked;
   wmeSpeedsUnverified = getId('_wmeSpeedsUnverifed').checked;
   wmeSpeedsHighlightOneWay = false;
+  var roadGroupSelector = document.getElementById('layer-switcher-group_road');
 
-  if (wmeSpeedsLayer.getVisibility()) {
+  if (wmeSpeedsLayer.getVisibility() && roadGroupSelector.checked) {
 
     var speedDiv = wmeSpeedsMiles ? 5 : 10;
     var maxSpeed = wmeSpeedsMiles ? wmeSpeedsMaxMphSpeed : wmeSpeedsMaxSpeed;
@@ -461,12 +462,20 @@ function getId(node) {
 }
 
 function changeLayer() {
-  localStorage.DrawSegmentSpeeds = wmeSpeedsLayer.getVisibility();
+  var roadGroupSelector = document.getElementById('layer-switcher-group_road');
+  var layerSelector = document.getElementById('layer-switcher-item_speeds');
 
-  if (wmeSpeedsLayer.getVisibility()) {
+  if (roadGroupSelector.checked && layerSelector.checked) {
     highlightSpeedsSegments();
   } else {
     wmeSpeedsLayer.removeAllFeatures();
+  }
+
+  if (layerSelector.checked) {
+    localStorage.DrawSegmentSpeeds = true;
+  }
+  else {
+    localStorage.DrawSegmentSpeeds = false;
   }
 }
 
@@ -563,7 +572,12 @@ function initialiseSpeedsHighlights() {
     checkbox.type = 'checkbox';
     checkbox.id = 'layer-switcher-item_speeds';
     checkbox.className = 'toggle';
-    checkbox.disabled = ! roadGroupSelector.checked;
+    checkbox.disabled = !roadGroupSelector.checked;
+
+    if (localStorage.DrawSegmentSpeeds === 'true') {
+      checkbox.checked = 'checked';
+    }
+
     togglerContainer.appendChild(checkbox);
     label.htmlFor = checkbox.id;
     labelText.className = 'label-text';
@@ -586,7 +600,7 @@ function initialiseSpeedsHighlights() {
   if (localStorage.DrawSegmentSpeeds === 'true') {
     wmeSpeedsLayer.setVisibility(localStorage.DrawSegmentSpeeds === 'true');
   } else {
-    wmeSpeedsLayer.setVisibility(true);
+    wmeSpeedsLayer.setVisibility(false);
   }
 
   // begin periodic updates
